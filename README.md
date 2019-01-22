8cb6f52f15989b1789eac5e427d16994d492edb8
TanguyScholtes <soultaker720@hotmail.com>
2019-01-22 10:45:11 +0100

    v1 of visit card

---
+++ README.md
@@ -0,0 +1,26 @@
+# Tanguy Scholtes - npx card
+
+> A CLI visit card - part of BeCode's studentship
+
+* * *
+
+
+## Installation
+
+No installation needed, as npx can use its black magic to do that for us. You'll need NPM installed for npx to work, though.
+
+
+## Usage
+
+Simply type in a terminal :
+
+```
+npx tanguyscholtes
+```
+
+and behold the awesomeness !
+
+
+## Part of BeCode's studentship
+
+I made this code as a trainee at [BeCode](https://becode.org/). If you'd like to check the exercise's specificities, you can [read it all here](https://github.com/becodeorg/LIE-Hamilton-1.7/tree/master/02-La-colline/02-NodeJS/01-cli) (it's only available in French, thought. Sorry pals !).
---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const chalk = require( 'chalk' );
+const figlet = require( 'figlet' );
+const boxen = require( 'boxen' );
+const gradient = require( 'gradient-string' );
+
+const boxenOptions = {
+    borderColor: '#2D3CD2',
+    borderStyle: 'round',
+    margin: {
+        top: 2,
+        bottom: 2,
+        left: 41,
+        right: 41
+    },
+    padding: 1
+};
+
+const datas = {
+    linebreak: '\n',
+    linkedin: chalk.blue.bold( 'Linkedin:' ) + chalk.white( ' https://www.linkedin.com/in/' ) + chalk.blue( 'tanguy-scholtes-907366172' ),
+    github: chalk.gray.bold( '           GitHub:' ) + chalk.white( ' https://github.com/' ) + chalk.cyan( 'TanguyScholtes' ),
+    npm: chalk.red.bold( "           NPM:" ) + chalk.white( ' https://www.npmjs.com/' ) + chalk.red( '~tanguyscholtes' ),
+    web: chalk.green.bold( '                 Web:' ) + chalk.green( ' http://tanguyscholtes.be/' )
+};
+
+const card = datas.linebreak +
+            datas.linkedin + datas.linebreak +
+            datas.github + datas.linebreak +
+            datas.npm + datas.linebreak +
+            datas.web + datas.linebreak;
+
+function displayString ( string, startColor, endColor, font ) {
+    figlet( string,
+        {
+            kerning: 'fitted',
+            font: font
+        },
+        function( error, string ) {
+            if ( error ) {
+                console.log( 'Whoops!' );
+                console.dir( error );
+                return;
+            }
+            console.log( gradient( startColor, endColor )( string ) );
+        }
+    );
+
+    return new Promise( success => {
+        setTimeout( () => {
+            success( 'displayed' );
+        }, 100 );
+    } );
+}
+
+async function displayCard () {
+    let name = await displayString( 'Tanguy Scholtes', 'rgb(185, 0, 0)', 'rgb(210, 145, 45)', 'Univers' );
+    let work = await displayString( "              Web developper", 'rgb(50, 130, 195)', 'rgb(45, 60, 210)', 'cybermedium' );
+
+    console.log( boxen( card, boxenOptions ) );
+}
+
+displayCard();
